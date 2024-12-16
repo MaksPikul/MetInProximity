9e3f53533c4761dec3d141afa98aaec31432d0d5
MaksPikul <mmaks522@gmail.com>
2024-12-16 15:28:43 +0000

    added rest of files

---
+++ MetInProximityBack/MetInProximityBack/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetInProximityBack.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        [HttpPost("googleLogin")]
+        public IActionResult googleOAuthLogin()
+        {
+            return View();
+        }
+
+        [HttpPost("microsoftLogin")]
+        public IActionResult microsoftOAuthLogin()
+        {
+            return View();
+        }
+
+        [HttpPost("logout")]
+        public IActionResult logout()
+        {
+            return View();
+        }
+
+        [HttpPost("ping")]
+        public IActionResult ping()
+        {
+            return View();
+        }
+    }
+}
